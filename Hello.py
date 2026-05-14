3192a30bb8529d915c99eeb94a1bf13af0f08ff6
Sai Vivek <sai.vivek@example.com>
2026-05-14 19:12:55 +0530

    student

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import pandas as pd
 df=pd.read_csv ('/Users/saivivek/Downloads/AD314_questionwise_data.csv')
 df1=pd.read_csv('/Users/saivivek/Downloads/AD314_mongo_data1.csv')
-# df1.drop(columns=['_id'],inplace=True)
+# df1.drop(cnnhholumns=['_id'],inplace=True)
 df=df.drop_duplicates()
 df1=df1.drop_duplicates()
 new_df=df.merge(df1,how='inner',on=['examid','deliveryid','admissionnumber','questionnumber'])
