e8e77bd1f8e8f2268a50bdb0a36425cc211b4eeb
Sai Vivek <sai.vivek@example.com>
2026-05-15 19:21:38 +0530

    new

---
+++ print.py
@@ -0,0 +1,8 @@
+print("hello world")
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+temp = float(input("enter the temperature: "))
+city = input("enter.  your city: ")
+
+boolean = bool(input("enter a boolean value (True/False): "))
+print("my name is " + name + " my age is " + str(age) + " and I live in " + city)
